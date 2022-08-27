954fd154d8dd0899c1b83e0ab1c6b3c5d4e4535f
namjiyun <namjiyun3275@gmail.com>
2022-08-28 03:04:03 +0900

    [FEAT] 사진 저장 API 작성

--- src/routes/QuestionRouter.ts
+++ src/routes/QuestionRouter.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import QuestionController from '../controllers/QuestionController';
 import auth from '../middlewares/auth';
+import upload from '../config/multer';
 const router = Router();
 
 router.post('/family', auth, QuestionController.postQuestion);
 router.get('/testQuestion', QuestionController.addQuestion);
+router.post('/list/:week', upload.single('file'), auth, QuestionController.addPhoto);
 router.get('/list', auth, QuestionController.getWeekList);
 
 router.get('/', auth, QuestionController.getQuestion);
