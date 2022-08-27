23bcd15bb93ca66922c550de9b71a4eacad0bb01
namjiyun <namjiyun3275@gmail.com>
2022-08-27 20:53:47 +0900

    [FEAT] 가족 참여 서비스 로직 구현

--- src/services/FamilyService.ts
+++ src/services/FamilyService.ts
@@ -12,6 +12,31 @@ const createFamily = async (client: any, code: string) => {
   return convertSnakeToCamel.keysToCamel(rows[0]);
 };
 
+const joinFamily = async (client: any, code: string, device: string) => {
+  const { rows } = await client.query(
+    `
+      SELECT *
+      FROM family
+      WHERE code = $1
+      `,
+    [code],
+  );
+  if (!rows[0]) {
+    throw 400;
+  }
+
+  const { rows: user } = await client.query(
+    `
+        INSERT INTO "user" (family_id, device)
+        VALUES ($1, $2)
+        RETURNING *
+        `,
+    [rows[0].id, device],
+  );
+  return convertSnakeToCamel.keysToCamel(user[0]);
+};
+
 export default {
   createFamily,
+  joinFamily,
 };
