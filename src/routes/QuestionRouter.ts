4d583af944cbd299e38cf826ba6daf3395ed0745
namjiyun <namjiyun3275@gmail.com>
2022-08-28 00:04:30 +0900

    [FEAT] 오늘 질문 조회 API 구현

--- src/routes/QuestionRouter.ts
+++ src/routes/QuestionRouter.ts
@@ -3,6 +3,7 @@ import QuestionController from '../controllers/QuestionController';
 import auth from '../middlewares/auth';
 const router = Router();
 
-router.get('/', auth, QuestionController.getQuestion);
 router.get('/testQuestion', QuestionController.addQuestion);
+router.get('/', auth, QuestionController.getQuestion);
+
 export default router;
