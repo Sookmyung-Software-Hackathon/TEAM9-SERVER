ebe013c0994fb928a055fb971cdbe48eed482eac
namjiyun <namjiyun3275@gmail.com>
2022-08-28 01:43:28 +0900

    [FEAT] 주차 수 조회 api 구현

--- src/routes/QuestionRouter.ts
+++ src/routes/QuestionRouter.ts
@@ -5,6 +5,8 @@ const router = Router();
 
 router.post('/family', auth, QuestionController.postQuestion);
 router.get('/testQuestion', QuestionController.addQuestion);
+router.get('/list', auth, QuestionController.getWeekList);
+
 router.get('/', auth, QuestionController.getQuestion);
 router.post('/', auth, QuestionController.postAnswer);
 export default router;
